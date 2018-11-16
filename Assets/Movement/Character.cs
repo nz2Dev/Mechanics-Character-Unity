008126986a469f1c355d298a00f17ce43f70fbab
Nazar <nz2develop@gmail.com>
2018-11-16 12:25:18 +0200

    Add character movement controller

---
+++ Assets/Movement/Character.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    private const float DampTime = 0.1f;
+
+    [SerializeField] private float stationarTurnSpeed = 180;
+    [SerializeField] private float movingTurnSpeed = 360;
+
+    private Animator animator;
+    private float turnAmount;
+    private float forwardAmount;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Move(Vector3 direction) {
+        if (direction.magnitude > 1) {
+            direction.Normalize();
+        }
+
+        direction = transform.InverseTransformDirection(direction);
+        turnAmount = Mathf.Atan2(direction.x, direction.z);
+        forwardAmount = direction.z;
+
+        ApplyExtraTurnRotation();
+        ApplyAnimation(direction);
+    }
+
+    private void ApplyAnimation(Vector3 direction) {
+        animator.SetFloat("forward", direction.z, DampTime, Time.deltaTime);
+        animator.SetFloat("turn", turnAmount, DampTime, Time.deltaTime);
+    }
+
+    private void ApplyExtraTurnRotation() {
+        float turnSpeed = Mathf.Lerp(stationarTurnSpeed, movingTurnSpeed, forwardAmount);
+        transform.Rotate(Vector3.up, turnAmount * Time.deltaTime * turnSpeed);
+    }
+
+}
---
+++ Assets/Movement/MovementController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    [SerializeField] private Character character;
+
+    public void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(horizontal, 0, vertical);
+        character.Move(moveVector);
+    }
+
+}
