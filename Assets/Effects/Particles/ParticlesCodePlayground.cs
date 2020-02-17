a3bb68eced231d33c91f5ba8192adbb4f65365e4
Nazar Pinonzhyk <nz2develop@gmail.com>
2020-02-17 15:44:19 +0200

    Add smoke particle emitter

---
+++ Assets/Effects/Particles/ParticlesCodePlayground.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class ParticlesCodePlayground : MonoBehaviour {
+
+        private void Awake() {
+            
+        }
+
+    }
+}
